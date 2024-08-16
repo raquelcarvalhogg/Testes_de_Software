5eb846ae2e3f793ecbb169c820522f8f9b55c2fe
raquelcarvalhogg <rachelcarvalho2006@gmail>
2024-08-16 15:19:15 -0300

    testes de comentarios

--- testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
@@ -2,9 +2,33 @@ describe('Página Principal', () => {
   beforeEach( () => {
     cy.visit('http://localhost:3000/')
   })
-  
   it('Deve renderizar h1 com o texto correto!', () => {
     cy.visit('http://localhost:3000')
-    cy.getByData('titulo-principal')
+    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira.Crie sua conta com a gente!')
   })
+
+  it('Deve renderizar h2 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('vantagnes') .contains('Vantagens do nosso banco')
+  })
+})
+
+it('Deve renderizar h3 com o texto correto!', () => {
+  cy.visit('http://localhost:3000')
+  cy.getByData('conta e cartao') .contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
+})
+
+it('Deve renderizar h3 com o texto correto!', () => {
+  cy.visit('http://localhost:3000')
+  cy.getByData('saques sem custo') .contains('Você pode sacar gratuitamente 4x por mês de qualquer Banco24h.')
+})
+
+it('Deve renderizar h3 com o texto correto!', () => {
+  cy.visit('http://localhost:3000')
+  cy.getByData('estilos vantagens') .contains(' Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!')
+})
+
+it('Deve renderizar h3 com o texto correto!', () => {
+  cy.visit('http://localhost:3000')
+  cy.getByData('seguro dispositivos') .contains('Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.')
 })
--- testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
+++ testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
@@ -12,21 +12,21 @@ export default function Inicio() {
       <div className={estilos.container}>
         <div className={estilos.conteudo}>
           <div className={estilos.banner}>
-            <h3>
+            <h1 data-test="titulo-principal">
               Experimente mais liberdade no controle da sua vida financeira.
               Crie sua conta com a gente!
-            </h3>
+            </h1>
             <img
               src={ilustracaoBanner}
               alt="Gráficos e uma pessoa com dinheiro na mão"
             />
           </div>
           <div className={estilos.vantagens}>
-            <h2>Vantagens do nosso banco:</h2>
+            <h2 data-test="vantagens">Vantagens do nosso banco:</h2>
             <div className={estilos.vantagens__itens}>
               <div className={estilos.vantagens__item}>
                 <img src={presente} alt="ícone de um presente" />
-                <h3>Conta e cartão gratuitos</h3>
+                <h3 data-test="conta e cartao">Conta e cartão gratuitos</h3>
                 <p>
                   Isso mesmo, nossa conta é digital, sem custo fixo e mais que
                   isso: sem tarifa de manutenção.
@@ -34,7 +34,7 @@ export default function Inicio() {
               </div>
               <div className={estilos.vantagens__item}>
                 <img src={saque} alt="ícone de uma mão segurando um dinheiro" />
-                <h3>Saques sem custo</h3>
+                <h3 data-test="saques sem custo">Saques sem custo</h3>
                 <p>
                   Você pode sacar gratuitamente 4x por mês de qualquer Banco
                   24h.
@@ -42,7 +42,7 @@ export default function Inicio() {
               </div>
               <div className={estilos.vantagens__item}>
                 <img src={estrela} alt="ícone de uma estrela" />
-                <h3>Programa de pontos</h3>
+                <h3 data-test= "estilos vantagens">Programa de pontos</h3>
                 <p>
                   Você pode acumular pontos com suas compras no crédito sem
                   pagar mensalidade!
@@ -53,7 +53,7 @@ export default function Inicio() {
                   src={dispositivo}
                   alt="ícone de um dispositivo como notebook"
                 />
-                <h3>Seguro Dispositivos</h3>
+                <h3 data-test= "seguro dispositivos">Seguro Dispositivos</h3>
                 <p>
                   Seus dispositivos móveis (computador e laptop) protegidos por
                   uma mensalidade simbólica.
@@ -66,3 +66,4 @@ export default function Inicio() {
     </>
   );
 }
+
