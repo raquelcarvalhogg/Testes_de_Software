4f1142454d73ced9419e51d998f807e7c4b472c0
raquelcarvalhogg <rachelcarvalho2006@gmail>
2024-08-22 14:04:02 -0300

    Validação do cadastro

--- testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
@@ -4,8 +4,8 @@ describe('Formulário cadastro', () => {
     })
     it('Usuário deve conseguir se cadastrar com sucesso', () => {
     cy.getByData('botao-cadastro').click() /* testaremos agora o botão 'cadastrar' */
-    cy.getByData('nome-input').type('Jose da Silva')
-    cy.getByData('email-input').type('za@email.com')
+    cy.getByData('nome-input').type('julio')
+    cy.getByData('email-input').type('julio@email.com')
     cy.getByData('senha-input').type('456789')
     cy.getByData('botao-enviar').click()
     cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso!')
