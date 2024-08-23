d3dc26b762ef466df7899d45e0cac66c5c5ad386
raquelcarvalhogg <rachelcarvalho2006@gmail>
2024-08-23 11:11:01 -0300

    Navegação entre páginas_2

--- testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
@@ -16,19 +16,25 @@ describe('Testando múltiplas páginas', () => {
 
 
     it('Deve conseguir acessar a página de cartões', () => {
+        cy.location('pathname').should('eq','/home')
         cy.getByData('app-home').find('a').eq(1).click()
         cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+        cy.location('pathname').should('eq','/home/cartoes')
     })
 
 
     it('Deve conseguir acessar a página de serviços', () => {
+        cy.location('pathname').should('eq','/home')
         cy.getByData('app-home').find('a').eq(2).click()
         cy.getByData('servicos').should('exist').and('have.descendants', 'h5', 'img')
+        cy.location('pathname').should('eq','/home/servicos')
     })
 
 
     it('Deve conseguir acessar a página de investimentos', () => {
+        cy.location('pathname').should('eq','/home')
         cy.getByData('app-home').find('a').eq(3).click()
         cy.getByData('title').should('exist').and('have.text', 'Investimentos')
+        cy.location('pathname').should('eq','/home/investimentos')
     })
 })
