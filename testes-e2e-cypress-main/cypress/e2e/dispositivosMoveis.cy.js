e99a227f97cf0771119a3194f210777e6e76244d
raquelcarvalhogg <rachelcarvalho2006@gmail>
2024-08-29 09:59:14 -0300

    definir a altura e largura.

--- testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
@@ -12,6 +12,50 @@ describe('Testando dispositivos móveis', () => {
         cy.getByData('menu-burguer').click()
         cy.getByData('menu-lateral').find('a').eq(3).click()			
 
-        cy.location('pathname').should('eq','/home/investimentos')			
+        cy.location('pathname').should('eq','/home/investimentos')
+    
+        cy.viewport(550, 750) 
+        cy.viewport('iphone-6', 'landscape')
+
+        
+		
     })
+})
+
+
+describe('Menu de navegação', () => {
+    context('Resolução de 720p', () => {
+      beforeEach(() => {
+
+        cy.viewport(1280, 720)
+      })
+
+      context('landscape ', () => {
+        beforeEach(() => {
+  
+          cy.viewport(1280, 720)
+        })
+      
+    })
+})
+
+
+
+    context('Resolução do iphone-5 ', () => {
+      beforeEach(() => {
+
+        cy.viewport('iphone-5')
+
+      })
+
+      context('Resolução do iphone-6 ', () => {
+        beforeEach(() => {
+  
+          cy.viewport('iphone-6')
+  
+        })
+  
+      
+    })
+  })
 })
