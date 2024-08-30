42d0a738c413c217574bb7fb60a4a3d72699e074
raquelcarvalhogg <rachelcarvalho2006@gmail>
2024-08-30 11:07:22 -0300

    nova funcionalidade

---
+++ testes-e2e-cypress-main/testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
@@ -0,0 +1,6 @@
+describe('Página Principal', () => {
+  it('Deve renderizar h1 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('[data-test="titulo-principal"]').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+  })
+})
